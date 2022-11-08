49b6eb39491fa033a823d668abfe49cab7003fa9
Philihp Busby <philihp@gmail.com>
2022-11-08 02:29:10 +0000

    create PCG-XSH-RR

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,9 +1,53 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { srand, rand } from '..'
+import { srand } from '..'
 
 describe('srand', () => {
-  it('is true', () => {
+  const randomizer = srand(42n)
+
+  it('returns an iterator', () => {
     expect.assertions(1)
-    expect(true).toBeTruthy()
+    expect(typeof randomizer.next).toBe('function')
+  })
+
+  it('returns an iterable', () => {
+    expect.assertions(1)
+    expect(typeof randomizer[Symbol.iterator]).toBe('function')
+  })
+
+  it('returns a generator', () => {
+    expect.assertions(1)
+    expect(randomizer[Symbol.iterator]()).toBe(randomizer)
+  })
+
+  it('can be used like an iterator', () => {
+    expect.assertions(2)
+    const randomizer = srand(42n)
+    const results = []
+    for (const num of randomizer) {
+      // infinite loop, unless you do this
+      if (results.length >= 10) break
+      results.push(num)
+    }
+    const { done } = randomizer.next()
+    expect(results).toStrictEqual([
+      11541586741780280317n,
+      1292176121501498n,
+      26704090232905n,
+      17854417029176992176n,
+      613727075605702022n,
+      51332072025775n,
+      8860481020046524554n,
+      3318766098841509934n,
+      30343170716585203n,
+      50682878230040n,
+    ])
+    expect(done).toBeTruthy()
+  })
+
+  it('behaves like an infinite generator', () => {
+    expect.assertions(2)
+    const { value, done } = randomizer.next()
+    expect(value).toBe(11541586741780280317n)
+    expect(done).toBeFalsy()
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export const srand = (_seed: number) => {}
-export const rand = (): number => 0
+// PCG-XSH-RR
+
+const mask = 0xffffffffffffffffn
+const increment = 1442695040888963407n
+const multiplier = 63641362238467932305n
+
+const rotr32 = (x: bigint, r: bigint) => (x >> r) | (x << (-r & 31n))
+
+export function* srand(seed: bigint) {
+  let state = seed + increment
+
+  while (true) {
+    let x = state
+    const count = x >> 59n
+    state = (x * multiplier + increment) & mask
+    x ^= x >> 18n
+    yield rotr32(x >> 27n, count)
+  }
+}
